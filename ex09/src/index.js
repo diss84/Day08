2106c3d0c81f977c230c635fbf93297b69a1a00b
Mirela <adisserdarevic@gmail.com>
2021-03-15 23:22:31 +0100

    Added file index.js

---
+++ ex09/src/index.js
@@ -0,0 +1,13 @@
+function isLess(a, b) {
+    return a < b;
+}
+// Change code below this line
+isLess(10, 15);
+isLess(15, 10);
+// Change code below this line
+
+
+console.log(isLess(10, 15));
+console.log(isLess(15, 10));
+
+module.exports = isLess;

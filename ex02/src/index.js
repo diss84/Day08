bd35cadeea2a36b0302e390ed45244e9dd3e78f5
Mirela <adisserdarevic@gmail.com>
2021-03-15 18:41:39 +0100

    Added file index.js

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+function functionPrintInfo(name, age) {
+// Write code below this line
+
+console.log(name, age);
+
+// Write code above this line
+}
+functionPrintInfo("John", 23);
+functionPrintInfo("Michael", 43);
+
+module.exports = functionPrintInfo;

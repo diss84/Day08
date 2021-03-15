18006d920d00ce5a0ef5aa7ba43615c7f3bb5ae7
Mirela <adisserdarevic@gmail.com>
2021-03-15 21:05:34 +0100

    Added file index.js

---
+++ ex05/src/index.js
@@ -0,0 +1,14 @@
+var dress = "little dress";
+
+function myDress() {
+    // Only change code below this line
+    var dress = "ball gown";
+    // Only change code below this line
+    return dress;
+}
+    // Only change code below this line
+myDress();
+    // Only change code above this line
+
+console.log(myDress(dress));
+module.exports = myDress;

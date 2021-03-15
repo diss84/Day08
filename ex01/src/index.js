7bdd5f3d18cd4273148bc952ea067c941221b195
Mirela <adisserdarevic@gmail.com>
2021-03-15 18:16:08 +0100

    Added file index.js

---
+++ ex01/src/index.js
@@ -0,0 +1,9 @@
+function reusableFunction() {
+        
+    //Write code below this line
+
+    console.log("Hi Padawan!");    
+
+    // Write code above this line
+}
+module.exports = reusableFunction();

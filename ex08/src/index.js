9d4310008ea087ac2d1e8f0a3b5792a971d0cf7e
Mirela <adisserdarevic@gmail.com>
2021-03-15 22:48:52 +0100

    Added file index.js

---
+++ ex08/src/index.js
@@ -0,0 +1,10 @@
+var total = 0;
+function funcWithArg(digit) {
+    return ( digit + 9) / 5;
+}
+    // Only change this code below
+total = funcWithArg(6);
+    // Only change this code above
+
+console.log(total);
+module.exports = funcWithArg;

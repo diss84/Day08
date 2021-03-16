b6c4cd42b56f805517367a0f835fd1937792b5fb
Mirela <adisserdarevic@gmail.com>
2021-03-16 01:18:53 +0100

    Added file index.js

---
+++ ex15/src/index.js
@@ -0,0 +1,9 @@
+function convertToInt(str) {
+    // Only change code below this line
+    return parseInt(str,  16);
+    // Only change code above this line
+}
+console.log(convertToInt("BA"));
+console.log(convertToInt("F1"));
+console.log(convertToInt("JeffBezos"));
+module.exports = convertToInt;
